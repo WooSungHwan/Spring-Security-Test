46bc9cf3f4b0734252ae2f74649c7cd55b7d190f
woo_private <doqndnffo@gmail.com>
2019-11-07 14:00:54 +0900

    Form을 이용한 로그인

--- src/main/java/com/sunghwan/example/config/SecurityConfig.java
+++ src/main/java/com/sunghwan/example/config/SecurityConfig.java
@@ -34,8 +34,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .anyRequest().authenticated()
                     .and()
                 .formLogin() //Form을 이용한 로그인을 사용하겠다.
+                    // 추가메소드
+                    /*
+                        successHandler
+                        : Login 성공시 추가 작업을 정의합니다.
+                        솔루션의 추가 인증이 필요한 경우 사용할 수 있습니다.
+                        SimpleUrlAuthenticationSuccessHandler 클래스를 extends 하거나
+                        AuthenticationSuccessHandler를 구현해서
+                        @Component로 정의한 후 autowired 해서 넣어주시면 됩니다.
+
+                        ailureHandler
+                        : Login 실패시 추가 작업을 정의할 수 있습니다.
+                        패스워드 몇회 이상 오류 시 계정 잠금 기능을 사용할 수 있습니다.
+                        SimpleUrlAuthenticationFailureHandler 클래스를 extends 하거나
+                        AuthenticationFailureHandler를 구현해서 사용하시면 됩니다.
+
+                     */
+
                     .and()
                 .logout();
+                    //추가 메소드
+                    /*
+                        logoutSuccessHandler : Logout 성공시 추가 작업을 정의합니다.
+                        1.a에서 솔루션 인증을 추가한 경우, 솔루션 Logout 처리를 해주면 좋을것 같습니다.
+                        마찬가지로 SimpleUrlLogoutSuccessHandler 클래스를 extends 하거나
+                        LogoutSuccessHandler를 구현해서 사용하시면 됩니다.
+                    */
     }
 
     @Override
